92c9c9ed172ef0a28c57bd57676205268a9b6caf
X04N <joancasasvidal@gmail.com>
2026-03-02 10:56:38 +0100

    Install marked library for proper markdown parsing

--- src/pages/BlogPostPage.tsx
+++ src/pages/BlogPostPage.tsx
@@ -1,34 +1,7 @@
 import { useParams, Link, Navigate } from 'react-router-dom';
 import { blogPosts } from '../data/blog';
 import { FaCalendar, FaUser, FaTag, FaArrowLeft } from 'react-icons/fa';
-
-// Simple markdown to HTML converter for basic formatting
-const markdownToHtml = (markdown: string): string => {
-  let html = markdown;
-  
-  // Headers
-  html = html.replace(/^### (.*$)/gim, '<h3>$1</h3>');
-  html = html.replace(/^## (.*$)/gim, '<h2>$1</h2>');
-  html = html.replace(/^# (.*$)/gim, '<h1>$1</h1>');
-  
-  // Bold
-  html = html.replace(/\*\*(.*?)\*\*/gim, '<strong>$1</strong>');
-  
-  // Italic
-  html = html.replace(/\*(.*?)\*/gim, '<em>$1</em>');
-  
-  // Links
-  html = html.replace(/\[([^\]]+)\]\(([^)]+)\)/gim, '<a href="$2">$1</a>');
-  
-  // Images
-  html = html.replace(/!\[([^\]]*)\]\(([^)]+)\)/gim, '<img src="$2" alt="$1" />');
-  
-  // Line breaks and paragraphs
-  html = html.replace(/\n\n/g, '</p><p>');
-  html = '<p>' + html + '</p>';
-  
-  return html;
-};
+import { marked } from 'marked';
 
 export const BlogPostPage = () => {
   const { slug } = useParams<{ slug: string }>();
@@ -105,7 +78,7 @@ export const BlogPostPage = () => {
             prose-li:my-2
             prose-img:rounded-lg prose-img:my-8
           "
-          dangerouslySetInnerHTML={{ __html: markdownToHtml(post.content) }}
+          dangerouslySetInnerHTML={{ __html: marked(post.content || '') }}
         />
 
         {/* CTA */}
